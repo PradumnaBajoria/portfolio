1ac4d40482ee74161da808c5e217c2c9eb29f67c
Pradumna <pradumnabajoria@gmail.com>
2021-06-22 19:24:45 +0530

    constructed different compent

---
+++ src/components/works/Works.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Works.scss"
+
+function Works() {
+    return (
+        <div className='works'>
+            Works
+        </div>
+    )
+}
+
+export default Works
