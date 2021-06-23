4f2acc9ff58ab353db0b1f1e3047f6430d7768ee
Pradumna <pradumnabajoria@gmail.com>
2021-06-23 23:15:03 +0530

    started with intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./Intro.scss"
+import Pic from "./../../assets/pic1.jpeg"
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Intro() {
     return (
         <div className='intro' id='intro'>
-            Intro
+            <div className="left">
+                <div className="imgContainer">
+                    <img src={Pic} alt="profile pic" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hey There, I'm</h2>
+                    <h1>Pradumna Bajoria</h1>
+                    <h3>Web <span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <ExpandMoreIcon className='downArrow' />
+                </a>
+            </div>
         </div>
     )
 }
