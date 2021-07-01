3cefcfbcfdf1c343d5d9236c5ff85a79b1d1570e
Pradumna <pradumnabajoria@gmail.com>
2021-07-01 23:46:36 +0530

    completed works section

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,32 +2,52 @@ import React from 'react'
 import "./Works.scss"
 import blog from "./../../assets/blog.png"
 import blogIcon from "./../../assets/blogIcon.jpg"
-
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { featuredPortfolio } from "./../../project"
+import { useState } from 'react';
 
 function Works() {
+
+    const [currentSlide, setCurrentSlide] = useState(0)
+
+    const data = featuredPortfolio
+
+    const handleClick = (param) => {
+        (param === "left") ? 
+            setCurrentSlide(
+                (currentSlide > 0) ? (currentSlide-1) : (data.length - 1)
+            ) : 
+            setCurrentSlide(
+                (currentSlide < (data.length - 1)) ? (currentSlide + 1) : 0
+            )
+    }
+
     return (
         <div className='works' id='works'>
-            <div className="slider">
-                <div className="container">
-                    <div className="item">
-                        <div className="left">
-                            <div className="leftContainer">
-                                <div className="imgContainer">
-                                    <img src={blogIcon} alt="img" />
+            <div className="slider" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
+                {data.map(item => (
+                    <div className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="leftContainer">
+                                    <div className="imgContainer">
+                                        <img src={item.icon} alt="img" />
+                                    </div>
+                                    <h2>{item.title}</h2>
+                                    <p>{item.desc}</p>
+                                    <span>Projects</span>
                                 </div>
-                                <h2>Title</h2>
-                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                                    fugiat perferendis tempora beatae vel sint eum in quia?
-                                </p>
-                                <span>Projects</span>
+                            </div>
+                            <div className="right">
+                                <img src={item.img} alt="img" />
                             </div>
                         </div>
-                        <div className="right">
-                            <img src={blog} alt="img" />
-                        </div>
                     </div>
-                </div>
+                ))}
             </div>
+            <ArrowBackIosIcon className="arrow left" onClick={() => handleClick("left")} />
+            <ArrowForwardIosIcon className="arrow right" onClick={() => handleClick("right")} />
         </div>
     )
 }
--- src/project.js
+++ src/project.js
@@ -1,5 +1,6 @@
 import blog from "./assets/blog.png"
 import ecom from "./assets/ecom.png"
+import icon from "./assets/blogIcon.jpg"
 
 // import blog from "./../../assets/blog.png"
 // import ecom from "./../../assets/ecom.png"
@@ -8,17 +9,23 @@ export const featuredPortfolio = [
     {
         id: "1",
         title: "Blog-App",
-        img: `${blog}`
+        img: `${blog}`,
+        desc: "Blog app created using mern. It has many functionality and can be used as anyregular blogging platform",
+        icon: `${icon}`
     },
     {
         id: "2",
         title: "E-Commerce-App",
-        img: `${ecom}`
+        img: `${ecom}`,
+        desc: "E-commerce platform created usong ReactJS and Stripe.",
+        icon: `${icon}`
     },
     {
         id: "3",
         title: "Online Notice Board",
-        img: `${blog}`
+        img: `${blog}`,
+        desc: "Online Notice Board System using LAMP technology",
+        icon: `${icon}`
     },
 ]
 
@@ -26,12 +33,16 @@ export const reactPortfolio = [
     {
         id: "1",
         title: "Blog-App",
-        img: `${blog}`
+        img: `${blog}`,
+        desc: "Blog app created using mern. It has many functionality and can be used as anyregular blogging platform",
+        icon: `${icon}`
     },
     {
         id: "2",
         title: "E-Commerce-App",
-        img: `${ecom}`
+        img: `${ecom}`,
+        desc: "E-commerce platform created usong ReactJS and Stripe.",
+        icon: `${icon}`
     },
 ]
 
@@ -39,21 +50,29 @@ export const otherPortfolio = [
     {
         id: "1",
         title: "Blog-App",
-        img: `${blog}`
+        img: `${blog}`,
+        desc: "Blog app created using mern. It has many functionality and can be used as anyregular blogging platform",
+        icon: `${icon}`
     },
     {
         id: "2",
         title: "E-Commerce-App",
-        img: `${ecom}`
+        img: `${ecom}`,
+        desc: "E-commerce platform created usong ReactJS and Stripe.",
+        icon: `${icon}`
     },
     {
         id: "3",
         title: "Online Notice Board",
-        img: `${blog}`
+        img: `${blog}`,
+        desc: "Blog app created using mern. It has many functionality and can be used as anyregular blogging platform",
+        icon: `${icon}`
     },
     {
         id: "4",
         title: "Spotify",
-        img: `${blog}`
+        img: `${blog}`,
+        desc: "Blog app created using mern. It has many functionality and can be used as anyregular blogging platform",
+        icon: `${icon}`
     }
 ]
